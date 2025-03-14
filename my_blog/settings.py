a893561e03020826d867f796430b0b2d5255f42b
parthiv-mcspl <113373062+parthiv-mcspl@users.noreply.github.com>
2025-03-14 12:01:05 +0530

    Update settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7=3lvn8hjkg!6-=+omr-3^saihjy6!q#xnm02p$l^ulkc9&!$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['creview-blog.onrender.com']
 
 
 # Application definition
